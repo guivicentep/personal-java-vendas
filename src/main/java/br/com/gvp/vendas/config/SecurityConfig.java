68c96a98157c5410df9a0825e85cd100e4622465
guivicentep <guivicente.gvp@hotmail.com>
2022-09-03 23:37:57 -0300

    feat: Implementação do UserDetailsService

--- src/main/java/br/com/gvp/vendas/config/SecurityConfig.java
+++ src/main/java/br/com/gvp/vendas/config/SecurityConfig.java
@@ -1,6 +1,8 @@
 package br.com.gvp.vendas.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -8,8 +10,13 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import br.com.gvp.vendas.service.impl.UsuarioServiceImpl;
+
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
+	
+	@Autowired @Lazy
+	private UsuarioServiceImpl usuarioService;
 	
 	@Bean
 	public PasswordEncoder passwordEncoder() {
@@ -18,11 +25,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication()
-				.passwordEncoder(passwordEncoder())
-				.withUser("adm")
-				.password(passwordEncoder().encode("123"))
-				.roles("USER", "ADMIN");
+		auth
+			.userDetailsService(usuarioService)
+			.passwordEncoder(passwordEncoder());
 	}
 	
 	@Override
@@ -37,6 +42,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 				.antMatchers("/api/pedidos/**")
 					.hasAnyRole("USER", "ADMIN")
 			.and()
-				.formLogin();
+				.httpBasic();
 	}
 }
