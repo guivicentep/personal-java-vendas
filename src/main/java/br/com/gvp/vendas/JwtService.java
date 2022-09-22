02758666a76e58a4b063efa5da7719a8473028d4
guivicentep <guivicente.gvp@hotmail.com>
2022-09-22 18:35:00 -0300

    feat: Decodificando o token

--- src/main/java/br/com/gvp/vendas/JwtService.java
+++ src/main/java/br/com/gvp/vendas/JwtService.java
@@ -10,6 +10,8 @@ import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.stereotype.Service;
 
 import br.com.gvp.vendas.entity.Usuario;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 
@@ -35,11 +37,42 @@ public class JwtService {
 					.compact();
 	}
 	
+	private Claims obterClaims(String token) throws ExpiredJwtException {
+		return Jwts
+					.parser()
+					.setSigningKey(keySignature)
+					.parseClaimsJws(token)
+					.getBody();
+	}
+	
+	public boolean tokenValido(String token) {
+		try {
+			Claims claims = obterClaims(token);
+			Date dataExpiracao = claims.getExpiration();
+			LocalDateTime data = 
+							dataExpiracao.toInstant()
+							.atZone(ZoneId.systemDefault()).toLocalDateTime();
+			
+			return !LocalDateTime.now().isAfter(data);
+		} catch (Exception e) {
+			return false;
+		}
+	}
+	
+	public String obterLoginUsuario(String token) throws ExpiredJwtException {
+		return (String) obterClaims(token).getSubject();
+	}
+	
 	public static void main(String[] args) {
 		ConfigurableApplicationContext contexto = SpringApplication.run(VendasApplication.class);
 		JwtService service = contexto.getBean(JwtService.class);
 		Usuario usuario = Usuario.builder().login("guizin").build();
 		String token = service.generateToken(usuario);
 		System.out.println(token);
+		
+		boolean isTokenValido = service.tokenValido(token);
+		System.out.println("O token está válido? " + isTokenValido);
+		
+		System.out.println(service.obterLoginUsuario(token));
 	}
 }
