08a089a7ac77b6817a520669b9a0e53b24856258
guivicentep <guivicente.gvp@hotmail.com>
2022-10-23 13:47:40 -0300

    feat: Finalizando a configuração do Swagger

--- src/main/java/br/com/gvp/vendas/controller/ClienteController.java
+++ src/main/java/br/com/gvp/vendas/controller/ClienteController.java
@@ -15,16 +15,21 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
 import br.com.gvp.vendas.entity.Cliente;
 import br.com.gvp.vendas.repository.ClientesRepository;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 
 @RestController
 @RequestMapping("/api/clientes")
+@Api("Api de Clientes")
 public class ClienteController {
 	
 	private ClientesRepository clienteRepository;
@@ -34,8 +39,13 @@ public class ClienteController {
 	}
 
 	@GetMapping("{id}")
-	@ResponseBody
-	public Cliente getClienteById(@PathVariable Integer id) {
+	@ApiOperation("Obter detalhes de um Cliente")
+	@ApiResponses({
+		@ApiResponse(code = 200, message = "Cliente encontrado"),
+		@ApiResponse(code = 404, message = "Cliente não encontrado para o ID informado")
+	})
+	public Cliente getClienteById(@PathVariable 
+			@ApiParam("Id do cliente") Integer id) {
 		return clienteRepository.findById(id)
 				.orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Cliente não encontrado"));
 		
@@ -43,6 +53,11 @@ public class ClienteController {
 	
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
+	@ApiOperation("Salva um novo cliente")
+	@ApiResponses({
+		@ApiResponse(code = 201, message = "Cliente salvo com sucesso"),
+		@ApiResponse(code = 400, message = "Erro de validação")
+	})
 	public Cliente save(@RequestBody @Valid Cliente cliente) {
 		return clienteRepository.save(cliente);
 	}
